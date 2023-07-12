b1675a2f6d6f71464c7e021cc7af5612ec8dcf2e
Miguel Michelson <miguelmichelson@gmail.com>
2023-07-12 01:58:13 -0400

    ci db connection

--- config/config.exs
+++ config/config.exs
@@ -4,13 +4,4 @@ config :autocontext, ecto_repos: [Autocontext.Repo]
 
 config :autocontext, repo: Autocontext.Repo
 
-config :autocontext, Autocontext.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER") || "postgres",
-  password: System.get_env("POSTGRES_PASSWORD") || "",
-  database: "autocontext_test",
-  hostname: "localhost",
-  poolsize: 10,
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # import_config "#{Mix.env()}.exs"
