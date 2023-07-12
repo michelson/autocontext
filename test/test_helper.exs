c2d7972b7803c3e549f1a5ac791d5cd6ea03f757
Miguel Michelson <miguelmichelson@gmail.com>
2023-07-12 02:08:27 -0400

    migrate db before tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,7 @@
 # ExUnit.start()
 
-Mix.Task.run("ecto.create", ~w(-r Autocontext.Repo))
-Mix.Task.run("ecto.migrate", ~w(-r Autocontext.Repo))
+# Mix.Task.run("ecto.create", ~w(-r Autocontext.Repo))
+# Mix.Task.run("ecto.migrate", ~w(-r Autocontext.Repo))
 
 Autocontext.Repo.start_link()
 Ecto.Adapters.SQL.Sandbox.mode(Autocontext.Repo, :manual)
